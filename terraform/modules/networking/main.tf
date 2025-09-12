3e4c8592c77922bab4c9b4d7bb6025051f49b4dd
schhin <schhin@gmail.com>
2025-09-12 13:04:43 -0400

    good for private-only VMs reaching Google APIs.

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -1,30 +1,63 @@
+############################
+# VPC (custom-mode baseline)
+############################
 resource "google_compute_network" "vpc" {
   project                 = var.project_id
   name                    = var.vpc_name
   auto_create_subnetworks = false
-  //labels                  = var.labels
+  routing_mode            = "REGIONAL"
+  # labels                = var.labels
 }
 
+#############################################
+# Subnets (PGA enabled + Flow Logs enabled)
+#############################################
 resource "google_compute_subnetwork" "subs" {
-  for_each                 = var.subnet_cidrs
+  for_each                 = var.subnet_cidrs           # e.g., { primary = "10.150.0.0/20" }
   project                  = var.project_id
   region                   = var.region
   name                     = "${var.vpc_name}-${each.key}"
   ip_cidr_range            = each.value
   network                  = google_compute_network.vpc.id
-  private_ip_google_access = true
+  private_ip_google_access = false
   stack_type               = "IPV4_ONLY"
   purpose                  = "PRIVATE"
-  //labels                           = var.labels
+
+  # VPC Flow Logs for visibility/troubleshooting
+  log_config {
+    aggregation_interval = "INTERVAL_5_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+    # metadata_fields    = []  # optional, provider >= 4.61
+  }
+  # labels               = var.labels
 }
 
+#############################################################
+# Proxy-only subnet (for Regional Internal HTTP(S) LB / PSC)
+#############################################################
+resource "google_compute_subnetwork" "proxy_only" {
+  count         = var.enable_regional_managed_proxy ? 1 : 0
+  project       = var.project_id
+  region        = var.region
+  name          = "${var.vpc_name}-proxy-only"
+  ip_cidr_range = var.proxy_only_cidr               # e.g., "10.0.1.0/24"
+  network       = google_compute_network.vpc.id
+  purpose       = "REGIONAL_MANAGED_PROXY"
+  role          = "ACTIVE"
+  stack_type    = "IPV4_ONLY"
+}
+
+############################
+# Cloud Router + Cloud NAT
+############################
 resource "google_compute_router" "r" {
   count   = var.enable_nat ? 1 : 0
   project = var.project_id
   region  = var.region
   name    = "${var.vpc_name}-router"
   network = google_compute_network.vpc.name
-  //labels  = var.labels
+  # labels = var.labels
 }
 
 resource "google_compute_router_nat" "nat" {
@@ -34,55 +67,106 @@ resource "google_compute_router_nat" "nat" {
   name                   = "${var.vpc_name}-nat"
   router                 = google_compute_router.r[0].name
   nat_ip_allocate_option = "AUTO_ONLY"
-  # Ensure no static addresses are reserved; keep NAT cheap/ephemeral.
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
   log_config {
     enable = var.nat_logging != "NONE"
-    filter = var.nat_logging
+    filter = var.nat_logging            # "ERRORS_ONLY" | "TRANSLATIONS_ONLY" | "ALL" | "NONE"
   }
 }
 
-# resource "google_compute_firewall" "allow_http" {
-#   name    = "${var.vpc_name}-allow-http"
-#   network = google_compute_network.vpc.name
-#   project = var.project_id
+####################
+# Firewall rules
+####################
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80"]
-#   }
+# Tight SSH
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "${var.vpc_name}-allow-ssh"
+  network = google_compute_network.vpc.name
+  project = var.project_id
 
-#   direction   = "INGRESS"
-#   source_ranges = [var.allowed_cidr]
-#   target_tags   = ["test-server1"]
-# }
+  direction     = "INGRESS"
+  priority      = 1000
+  source_ranges = [var.allowed_cidr]
 
-# resource "google_compute_firewall" "allow_https" {
-#   name    = "${var.vpc_name}-allow-https"
-#   network = google_compute_network.vpc.name
-#   project = var.project_id
+  target_tags = var.target_tags # ["test-server1"] or similar
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["443"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
 
-#   direction   = "INGRESS"
-#   source_ranges = [var.allowed_cidr]
-#   target_tags   = ["test-server1"]
-# }
+# Optional ICMP from trusted range
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "${var.vpc_name}-allow-icmp"
+  network = google_compute_network.vpc.name
+  project = var.project_id
 
-# resource "google_compute_firewall" "allow_ssh" {
-#   name    = "${var.vpc_name}-allow-ssh"
-#   network = google_compute_network.vpc.name
-#   project = var.project_id
+  direction     = "INGRESS"
+  priority      = 1000
+  source_ranges = [var.allowed_cidr]
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
+  allow {
+    protocol = "icmp"
+  }
+}
 
-#   direction   = "INGRESS"
-#   source_ranges = [var.allowed_cidr]
-#   target_tags   = ["test-server1"]
-# }
+# HTTP/HTTPS (only if you truly need them)
+resource "google_compute_firewall" "allow_http" {
+  count   = var.enable_http ? 1 : 0
+  name    = "${var.vpc_name}-allow-http"
+  network = google_compute_network.vpc.name
+  project = var.project_id
+
+  direction     = "INGRESS"
+  priority      = 1000
+  source_ranges = [var.allowed_cidr]
+  target_tags   = var.target_tags
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+}
+
+resource "google_compute_firewall" "allow_https" {
+  count   = var.enable_https ? 1 : 0
+  name    = "${var.vpc_name}-allow-https"
+  network = google_compute_network.vpc.name
+  project = var.project_id
+
+  direction     = "INGRESS"
+  priority      = 1000
+  source_ranges = [var.allowed_cidr]
+  target_tags   = var.target_tags
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+}
+
+# Intra-VPC (limit to your subnets, not 0.0.0.0/0)
+resource "google_compute_firewall" "allow_internal" {
+  name    = "${var.vpc_name}-allow-internal"
+  network = google_compute_network.vpc.name
+  project = var.project_id
+
+  direction     = "INGRESS"
+  priority      = 65534
+  source_ranges = values(var.subnet_cidrs)   # all the CIDRs you defined
+
+  allow { 
+    protocol = "tcp"
+    ports = ["0-65535"] 
+  }
+
+  allow { 
+    protocol = "udp"
+    ports = ["0-65535"] 
+  }
+  allow { 
+    protocol = "icmp" 
+  }
+}
--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -28,7 +28,7 @@ variable "labels" {
 }
 
 variable "nat_logging" {
-  description = "Cloud NAT logging filter: NONE | ERRORS_ONLY | ALL"
+  description = "Cloud NAT logging filter: NONE | ERRORS_ONLY | TRANSLATIONS_ONLY | ALL"
   type        = string
   default     = "ERRORS_ONLY"
 }
@@ -47,15 +47,49 @@ variable "region" {
   description = "The region to deploy resources"
   type        = string
 }
+variable "project_id" { type = string }
+variable "vpc_name"    { type = string }
+variable "region"      { type = string }
+
+# Map of subnet name -> CIDR, e.g. { primary = "10.150.0.0/20" }
+variable "subnet_cidrs" {
+  type = map(string)
+}
+
+variable "allowed_cidr" {
+  description = "Trusted source CIDR for admin ingress (SSH/ICMP/HTTP/HTTPS if enabled)"
+  type        = string
+}
+
+variable "target_tags" {
+  description = "Network tags applied to instances that should receive the ingress rules"
+  type        = list(string)
+  default     = []
+}
 
 variable "enable_nat" {
-  description = "Enable Cloud NAT"
+  type    = bool
+  default = false
+}
+# variable "nat_logging" { type = string default = "ERRORS_ONLY" } # NONE | ERRORS_ONLY | TRANSLATIONS_ONLY | ALL
+
+variable "enable_regional_managed_proxy" {
+  description = "Create a REGIONAL_MANAGED_PROXY subnet (needed for Regional Internal HTTP(S) LB / PSC)"
   type        = bool
   default     = false
 }
 
-variable "allowed_cidr" {
-  description = "CIDR allowed to reach HTTP/HTTPS on the VM"
+variable "proxy_only_cidr" {
+  description = "CIDR for proxy-only subnet (non-routable to VMs)"
   type        = string
-  default     = "0.0.0.0/0"
+  default     = "10.0.1.0/24"
+}
+
+variable "enable_http" {
+  type    = bool
+  default = false
+}
+variable "enable_https" {
+  type    = bool
+  default = false
 }
